88f99ea8bec0554e74692ca120beb17e329ee2b9
OjaanIr <farnasrozaan7@gmail.com>
2022-03-16 23:03:31 +0700

    implement bfs and dfs folder crawler (not tested)

---
+++ src/dfs_folder_crawler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using Sysem.Collections.Generic;
+
+public class DFS
+{
+    // Traverse a given directory using DFS to find specified file
+    public static void folder_crawling(string start, string find)
+    {
+        // get the list of all files and directories in starting directory
+        string[] list_of_files_and_directories = Directory.GetFileSystemEntries(start, "*", SearchOption.TopDirectoryOnly);
+
+        // check whether list_of_files_and_directories is null or not
+        if (list_of_files_and_directories != null)
+        {
+            // iterate every files and directories in the list_of_files_and_directories
+            foreach (string path in list_of_files_and_directories)
+            {
+                // if the current path is a directory, recur for it
+                if (Directory.Exists(path))
+                {
+                    folder_crawling(path);
+                }
+                else
+                {
+                    // if file found write "File found!", otherwise write "File not found!"
+                    if (find.Equals(path))
+                    {
+                        Console.WriteLine("File found!")
+                    }
+                    else
+                    {
+                        Console.WriteLine("File not found!")
+                    }
+                }
+            }
+        }
+    }
+}
